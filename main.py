e7b57052c1b7f8f8c769c3ccbac0cfc72ec6e747
Nikita <mordnikita17@gmail.com>
2025-09-18 09:55:35 +0500

    fix: main

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import uvicorn
 from api import create_app
 
 app = create_app()
-uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
